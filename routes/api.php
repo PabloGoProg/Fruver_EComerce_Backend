671b9ba993b0e4a72c72d389a18e6ea212930ff5
PabloGoProg <pgosorio13@gmail.com>
2023-12-02 17:20:35 -0500

    api change

--- routes/api.php
+++ routes/api.php
@@ -23,9 +23,15 @@ Route::middleware('auth:api')->group(function () {
     Route::get('/v1/users/{id}', [App\Http\Controllers\api\v1\UserController::class, 'show']);
     Route::put('/v1/users/{id}', [App\Http\Controllers\api\v1\UserController::class, 'update']);
     Route::delete('/v1/users/{id}', [App\Http\Controllers\api\v1\UserController::class, 'destroy']);
-});
 
+    Route::get('/v1/suppliers', [App\Http\Controllers\api\v1\SupplierController::class, 'index']);
+    Route::get('/v1/suppliers/{id}', [App\Http\Controllers\api\v1\SupplierController::class, 'show']);
+    Route::post('/v1/suppliers', [App\Http\Controllers\api\v1\SupplierController::class, 'store']);
+    Route::put('/v1/suppliers/{id}', [App\Http\Controllers\api\v1\SupplierController::class, 'update']);
+    Route::delete('/v1/suppliers/{id}', [App\Http\Controllers\api\v1\SupplierController::class, 'destroy']);
+});
 Route::group([
+
 
     'middleware' => 'api',
     'prefix' => '/v1/auth'
@@ -49,9 +55,3 @@ Route::get('/v1/user_types/{id}', [App\Http\Controllers\api\v1\UserTypeControlle
 Route::post('/v1/user_types', [App\Http\Controllers\api\v1\UserTypeController::class, 'store']);
 Route::put('/v1/user_types/{id}', [App\Http\Controllers\api\v1\UserTypeController::class, 'update']);
 Route::delete('/v1/user_types/{id}', [App\Http\Controllers\api\v1\UserTypeController::class, 'destroy']);
-
-Route::get('/v1/suppliers', [App\Http\Controllers\api\v1\SupplierController::class, 'index']);
-Route::get('/v1/suppliers/{id}', [App\Http\Controllers\api\v1\SupplierController::class, 'show']);
-Route::post('/v1/suppliers', [App\Http\Controllers\api\v1\SupplierController::class, 'store']);
-Route::put('/v1/suppliers/{id}', [App\Http\Controllers\api\v1\SupplierController::class, 'update']);
-Route::delete('/v1/suppliers/{id}', [App\Http\Controllers\api\v1\SupplierController::class, 'destroy']);
